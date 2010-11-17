342e43c98051bbcde08d7027d0f9f0a05fffbe5d
insidepower <insidepower@insidepower-laptop.(none)>
2010-11-17 09:59:34 +0800

    make execBash more generic and template use execBash

--- execBash.py
+++ execBash.py
@@ -2,12 +2,24 @@
 import subprocess
 
 ## execute the bash command
-def execBash(cmd):
-	print cmd
+#def execBash(cmd):
+#	print cmd
+#	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+#	out = p.stdout.read()
+#	#print out
+#	return out
+def execBash(cmd, is_suppress=False):
+	#print cmd
 	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-	out = p.stdout.read()
+	#(stdoutdata, stderrdata) = p.communicate()
+	stdoutdata = p.communicate()[0]
+	p.poll()
+	if (p.returncode != 0) and (not is_suppress):
+		print ("\033[1;31mWarning!! command (%s) not ended properly."
+				"exit status = %d\033[0m" %(cmd, p.returncode))
+	#out = p.stdout.read()
 	#print out
-	return out
+	return (stdoutdata, p.returncode)
 
 ## if standalone, i.e. called directly from shell
 if __name__ == '__main__':
