136c7d3e78a2f1c2e1f837ec97aa274825b4d823
ken <mailingonly@gmail.com>
2014-01-06 07:30:50 +0800

    added pyvimsession and pyvimload.py

--- pyvimsession.py
+++ pyvimsession.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python
 '''
-usage: to load the android files to vim
+usage: to load vim session file
 '''
+
 #----------------- import ----------------#
 import optparse
 import subprocess
 import socket
+import os
+import getpass
+import sys
 
 #----------------- global (user customizable) variable ----------------#
 xml_cmd='find . -name "*.xml" | /bin/egrep -v bin | /bin/egrep -v values-v*'
 java_cmd='find . -name "*.java" | /bin/egrep -v R.java\|BuildConfig.java'
 load_vim='gvim --servername %s --remote ' % (socket.gethostname())
+dir_vim_session=os.path.join("/home", getpass.getuser(),"kn_vimsession")
+check_dir_exist="ls %s" % (dir_vim_session)
+vim_cmd='ls %s%s*.vims' % (dir_vim_session, os.sep)
+vim_session="gvim --servername %s -S " % (socket.gethostname())
+load_vim_session=vim_session+" %s &"
 
 #----------------- parseCmd() ----------------#
 ## parse the command line arguments
@@ -44,33 +53,38 @@ def main():
 	#(options, args) = parseCmd()
 	result_arr = []
 
-	result_xml = execBash(xml_cmd)[0];
-	result_java = execBash(java_cmd)[0];
+	result_dir_exist = execBash(check_dir_exist, True)[1];
+	#print result_dir_exist
+	if (0!=result_dir_exist):
+			print("%s is not exist. Program Ended." % (dir_vim_session))
+			sys.exit(2)
 
-	result_arr = result_xml.split();
-	result_arr.extend(result_java.split());
+	result_vim = execBash(vim_cmd, True)[0]
+	#result_xml = execBash(xml_cmd)[0];
+	#result_java = execBash(java_cmd)[0];
+
+	if ( ""==result_vim ):
+			print("No vim session (.vims) found. Program Ended.")
+			sys.exit(2)
+
+	result_arr = result_vim.split();
+	#result_arr.extend(result_java.split());
 
 	for i, line in enumerate(result_arr):
 		print ("%d - %s" % (i, line))
 
-	print("\nDocument(s) to load. *(all), j(java), x(xml), c(cancel)")
+	print("\nDocument(s) to load. *(all), c(cancel)")
 	print("Multiple documents separated by space, e.g.: 3 5\n")
 	choices= raw_input("Doc(s) to load : ");
 
 	if ( choices == "*"):
 		for myfile in result_arr:
-			execBash(load_vim+myfile, is_print=True)[0];
-	elif ( choices == "x"):
-		for i, myfile in enumerate(result_xml.split()):
-			execBash(load_vim+myfile, is_print=True)[0];
-	elif ( choices == "j"):
-		for i, myfile in enumerate(result_java.split()):
-			execBash(load_vim+myfile, is_print=True)[0];
+			execBash(load_vim_session % myfile, is_print=True)[0];
 	elif ( choices == "c"):
 		print("Operation cancelled")
 	else:
 		for choice in choices.split():
-			execBash(load_vim+result_arr[int(choice)], is_print=True)[0];
+			execBash(load_vim_session % result_arr[int(choice)], is_print=True)[0];
 	
 
 #----------------- standalone() ----------------#
